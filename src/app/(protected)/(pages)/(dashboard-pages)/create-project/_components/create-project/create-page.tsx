96757f4f1a3f88213c1a5c83b9e2fee5acc47fa6
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-02-12 23:46:49 +0530

    style: create project page options

---
+++ src/app/(protected)/(pages)/(dashboard-pages)/create-project/_components/create-project/create-page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  containerVariants,
+  CreatePageCard,
+  itemVariants,
+} from "@/lib/constants";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+
+type Props = {
+  onSelectOption: (option: string) => void;
+};
+
+const CreatePage = ({ onSelectOption }: Props) => {
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="space-y-8"
+    >
+      <motion.div variants={itemVariants} className="space-y-2 text-center">
+        <h1 className="text-4xl font-bold text-primary">
+          How would you like to get started?
+        </h1>
+        <p className="text-secondary">Choose your preffered method to begin</p>
+      </motion.div>
+      <motion.div
+        variants={containerVariants}
+        className="grid gap-6 md:grid-cols-3"
+      >
+        {CreatePageCard.map((option) => (
+          <motion.div
+            key={option.type}
+            variants={itemVariants}
+            whileHover={{
+              scale: 1.05,
+              rotate: 1,
+              transition: { duration: 0.1 },
+            }}
+            className={cn(
+              "rounded-xl p-[1px] transition-all duration-200 ease-in-out",
+              {
+                "bg-presentiq-gradient": option.highlight,
+                "border hover:bg-presentiq-gradient": !option.highlight,
+              },
+            )}
+          >
+            <motion.div
+              className="flex w-full flex-col items-start gap-y-6 rounded-xl bg-white p-4 dark:bg-black"
+              whileHover={{
+                transition: { duration: 0.1 },
+              }}
+            >
+              <div className="flex w-full flex-col items-start gap-y-3">
+                <div>
+                  <p className="text-lg font-semibold text-primary">
+                    {option.title}
+                  </p>
+                  <p
+                    className={cn("text-4xl font-bold", {
+                      "text-presentiq": option.highlight,
+                      "text-primary": !option.highlight,
+                    })}
+                  >
+                    {option.highlightedText}
+                  </p>
+                </div>
+                <p className="text-sm font-normal text-secondary">
+                  {option.description}
+                </p>
+              </div>
+              <motion.div
+                className="self-end"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Button
+                  variant={option.highlight ? "default" : "outline"}
+                  size={"sm"}
+                  onClick={() => onSelectOption(option.type)}
+                >
+                  {option.highlight ? "Generate" : "Continue"}
+                </Button>
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default CreatePage;
--- src/app/(protected)/(pages)/(dashboard-pages)/create-project/_components/render-page.tsx
+++ src/app/(protected)/(pages)/(dashboard-pages)/create-project/_components/render-page.tsx
@@ -3,10 +3,25 @@
 import { usePromptStore } from "@/hooks/use-prompt-store";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import CreatePage from "./create-project/create-page";
 
 const RenderPage = () => {
   const router = useRouter();
   const { page, setPage } = usePromptStore();
+
+  const renderStep = () => {
+    switch (page) {
+      case "create":
+        return <CreatePage />;
+      case "create-scratch":
+        return <></>;
+      case "creative-ai":
+        return <></>;
+
+      default:
+        return null;
+    }
+  };
 
   return (
     <AnimatePresence mode="wait">
@@ -16,7 +31,9 @@ const RenderPage = () => {
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: -20 }}
         transition={{ duration: 0.3 }}
-      ></motion.div>
+      >
+        {renderStep()}
+      </motion.div>
     </AnimatePresence>
   );
 };
