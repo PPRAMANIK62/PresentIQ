8875cb87574e597403587dbb1fb763142d12e814
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-02-12 17:58:29 +0530

    feat: working on project recover from trash

--- src/components/global/project-card/index.tsx
+++ src/components/global/project-card/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import AlertDialogBox from "@/components/global/alert-dialog-box";
+import { Button } from "@/components/ui/button";
 import { useSlideStore } from "@/hooks/use-slide-store";
+import { useToast } from "@/hooks/use-toast";
 import { itemVariants, themes } from "@/lib/constants";
 import { timeAgo } from "@/lib/timeAgo";
 import { type Slide } from "@/lib/types";
@@ -8,6 +11,7 @@ import { cn } from "@/lib/utils";
 import { type JsonValue } from "@prisma/client/runtime/library";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import ThumbnailPreview from "./thumbnail-preview";
 
 type Props = {
@@ -29,8 +33,12 @@ const ProjectCard = ({
   src,
   themeName,
 }: Props) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+
   const { setSlides } = useSlideStore();
   const router = useRouter();
+  const { toast } = useToast();
 
   const theme = themes.find((theme) => theme.name === themeName) ?? themes[0]!;
   // const slides = JSON.parse(JSON.stringify(slideData)) as Slide[];
@@ -40,11 +48,30 @@ const ProjectCard = ({
     router.push(`/presentation/${projectId}`);
   };
 
+  const handleRecover = async () => {
+    setLoading(true);
+    if (!projectId) {
+      setLoading(false);
+      toast({
+        title: "Error",
+        description: "Project not found",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    try {
+    } catch (error) {
+      console.error(error);
+      return { status: 500, error: "Internal Server Error" };
+    }
+  };
+
   return (
     <motion.div
       variants={itemVariants}
       className={cn(
-        "group flex w-full flex-col gap-y-3 rounded-xl p-3 transition-colors",
+        "group flex w-full flex-col gap-y-1 rounded-xl p-3 transition-colors",
         {
           "hover:bg-muted/50": !isDeleted,
         },
@@ -63,7 +90,7 @@ const ProjectCard = ({
       <div className="w-full">
         <div className="space-y-1">
           <h3 className="line-clamp-1 text-base font-semibold text-primary">
-            {title}
+            {title}This is a mock title given by me for now.
           </h3>
           <div className="flex w-full items-center justify-between gap-2">
             <p
@@ -72,6 +99,27 @@ const ProjectCard = ({
             >
               {timeAgo(createdAt)}
             </p>
+            {/* {isDeleted ? ( */}
+            <AlertDialogBox
+              description="This will recover your project and restore your data."
+              className="bg-green-500 text-white hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700"
+              loading={loading}
+              open={open}
+              onClick={handleRecover}
+              handleOpen={() => setOpen(!open)}
+            >
+              <Button
+                size={"sm"}
+                variant={"ghost"}
+                className="bg-background-80 dark:hover:bg-background-70"
+                disabled={loading}
+              >
+                Recover
+              </Button>
+            </AlertDialogBox>
+            {/* ) : ( */}
+            {/* "" */}
+            {/* )} */}
           </div>
         </div>
       </div>
